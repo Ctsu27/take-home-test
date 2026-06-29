515efd5a0d247ecba880efb4b68db6ae34ec40d7
kehuang <36479937+Ctsu27@users.noreply.github.com>
2026-06-29 11:36:47 +0200

    feat-test -> add test for the base drugs

--- pharmacy.test.js
+++ pharmacy.test.js
@@ -2,8 +2,38 @@ import { Drug, Pharmacy } from "./pharmacy";
 
 describe("Pharmacy", () => {
   it("should decrease the benefit and expiresIn", () => {
-    expect(new Pharmacy([new Drug("test", 2, 3)]).updateBenefitValue()).toEqual(
-      [new Drug("test", 1, 2)],
+    expect(new Pharmacy([new Drug("Doliprane", 20, 30)]).updateBenefitValue()).toEqual(
+      [new Drug("Doliprane", 19, 29)],
+    );
+  });
+  it("should decrease the benefit and expiresIn", () => {
+    expect(new Pharmacy([new Drug("Doliprane", -1, 15)]).updateBenefitValue()).toEqual(
+      [new Drug("Doliprane", -2, 13)],
+    );
+  });
+  it("should increase the benefit and decrease expiresIn", () => {
+    expect(new Pharmacy([new Drug("Herbal Tea", 10, 5)]).updateBenefitValue()).toEqual(
+      [new Drug("Herbal Tea", 9, 6)],
+    );
+  });
+  it("should increase the benefit and decrease expiresIn", () => {
+    expect(new Pharmacy([new Drug("Herbal Tea", -3, 37)]).updateBenefitValue()).toEqual(
+      [new Drug("Herbal Tea", -4, 39)],
+    );
+  });
+  it("should increase the benefit and decrease expiresIn", () => {
+    expect(new Pharmacy([new Drug("Fervex", 13, 23)]).updateBenefitValue()).toEqual(
+      [new Drug("Fervex", 12, 24)],
+    );
+  });
+  it("should increase the benefit and decrease expiresIn", () => {
+    expect(new Pharmacy([new Drug("Fervex", 8, 37)]).updateBenefitValue()).toEqual(
+      [new Drug("Fervex", 7, 39)],
+    );
+  });
+  it("should not change the benefit and expiresIn", () => {
+    expect(new Pharmacy([new Drug("Magic Pill", 13, 23)]).updateBenefitValue()).toEqual(
+      [new Drug("Magic Pill", 13, 23)],
     );
   });
 });
